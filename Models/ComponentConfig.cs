b7ebfb5b09c196aaf7e82701da4b30ef32b29118
enzobz <112773711+enzobz@users.noreply.github.com>
2026-02-17 10:21:15 -0300

    Create ComponentConfig class representing individual component configuration

---
+++ Models/ComponentConfig.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SolidEdgeConfigurator.Models
+{
+    public class ComponentConfig
+    {
+        public string ComponentName { get; set; }
+        public bool IsVisible { get; set; }
+        public string ConfigurationName { get; set; }
+        public string Description { get; set; }
+    }
+}
